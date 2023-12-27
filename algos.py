866f82e9f6ac40688a7b941be3fa0c2778213fa4
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-27 20:32:52 +0200

    Fix R2 calculation

--- algos.py
+++ algos.py
@@ -2,7 +2,7 @@ from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR, SVC
-from sklearn.metrics import mean_squared_error, accuracy_score
+from sklearn.metrics import mean_squared_error, accuracy_score, r2_score
 from math import sqrt
 
 
@@ -10,8 +10,8 @@ def knn_regression(k, x_train, y_train, x_test, y_test):
     knn = KNeighborsRegressor(n_neighbors = k)
     knn.fit(x_train, y_train)
     predictions = knn.predict(x_test)
-    r = sqrt(mean_squared_error(y_test, predictions))
-    return predictions, r
+    r2 = r2_score(y_test, predictions)
+    return predictions, r2
 
 
 def knn_classification(k, x_train, y_train, x_test, y_test):
@@ -26,8 +26,8 @@ def svm_regression(x_train, y_train, x_test, y_test):
     svr = SVR()
     svr.fit(x_train, y_train)
     predictions = svr.predict(x_test)
-    r = sqrt(mean_squared_error(y_test, predictions))
-    return predictions, r
+    r2 = r2_score(y_test, predictions)
+    return predictions, r2
 
 
 def svm_classification(x_train, y_train, x_test, y_test):
@@ -42,16 +42,16 @@ def linear_regression(x_train, y_train, x_test, y_test):
     lr = LinearRegression()
     lr.fit(x_train, y_train)
     predictions = lr.predict(x_test)
-    r = sqrt(mean_squared_error(y_test, predictions))
-    return predictions, r
+    r2 = r2_score(y_test, predictions)
+    return predictions, r2
 
 
 def decision_tree_regression(x_train, y_train, x_test, y_test):
     tree = DecisionTreeRegressor()
     tree.fit(x_train, y_train)
     predictions = tree.predict(x_test)
-    r = sqrt(mean_squared_error(y_test, predictions))
-    return predictions, r
+    r2 = r2_score(y_test, predictions)
+    return predictions, r2
 
 
 def decision_tree_classification(x_train, y_train, x_test, y_test):
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from algos import *
 
 x_train, y_train, x_test, y_test = split_data(data_set_path = "Battery_RUL.csv", train_percent = 0.8)
 
-predictions, r = knn_regression(3, x_train, y_train, x_test, y_test)
-print(r)
+predictions, r2 = knn_regression(3, x_train, y_train, x_test, y_test)
+print(r2)
 
-predictions, r = svm_regression(x_train, y_train, x_test, y_test)
-print(r)
+predictions, r2 = svm_regression(x_train, y_train, x_test, y_test)
+print(r2)
 
-predictions, r = linear_regression(x_train, y_train, x_test, y_test)
-print(r)
+predictions, r2 = linear_regression(x_train, y_train, x_test, y_test)
+print(r2)
 
-predictions, r = decision_tree_regression(x_train, y_train, x_test, y_test)
-print(r)
+predictions, r2 = decision_tree_regression(x_train, y_train, x_test, y_test)
+print(r2)
