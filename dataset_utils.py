69f0342e66c21ce78c56dc60c519103857c3ff86
Mahmoud Ibrahim <84087861+3wiida@users.noreply.github.com>
2023-12-28 02:22:26 +0200

    knn

--- dataset_utils.py
+++ dataset_utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import KBinsDiscretizer
 
 # 15064 rows
 # data = pd.read_csv("Battery_RUL.csv")
@@ -17,3 +19,17 @@ def split_data(data_set_path, train_percent):
     y_test = data.iloc[train_end_index:, -1].values
     
     return x_train, y_train, x_test, y_test
+
+def discretize_dataset(data_set_path, train_percent, n_bins=5, encode='ordinal', strategy='uniform'):
+    datafram = pd.read_csv(data_set_path)
+    discretizer = KBinsDiscretizer(n_bins=n_bins, encode=encode, strategy=strategy,subsample=None)
+    data = pd.DataFrame(discretizer.fit_transform(datafram), columns=datafram.columns)
+    rows = data.shape[0]
+    train_end_index = round(train_percent * rows)
+    x_train = data.iloc[:train_end_index, :-1].values
+    y_train = data.iloc[:train_end_index, -1].values
+    
+    x_test = data.iloc[train_end_index:, :-1].values
+    y_test = data.iloc[train_end_index:, -1].values
+    
+    return x_train, y_train, x_test, y_test 
